e20fc9b28341029fd13ff1c43347ad3da5b8daf3
Johan Halin <johan.halin@karppinen.fi>
2010-04-03 19:58:43 +0300

    first commit, nothing done yet

---
+++ Classes/ControlAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ControlAppDelegate.h
+//  Control
+//
+//  Created by Johan Halin on 03.04.2010.
+//  Copyright Parasol 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControlAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Control
+//
+//  Created by Johan Halin on 03.04.2010.
+//  Copyright Parasol 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"ControlAppDelegate");
+	[pool release];
+	return retVal;
+}
